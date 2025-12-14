c1dc143a49891ecd424959c9d274758a7039b828
Najam <realnajmiter@gmail.com>
2025-12-14 23:52:22 +0500

    components & jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,43 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+// class
+// function
+
+// components, jsx
+
+export default function App() {
+  /**
+    1. component (dom, user defined)
+    2. number, string, boolean, undefined, null, arrays
+  */
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite - React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <main>
+      <p>i'm also here</p>
+      <MyName></MyName>
+    </main>
   );
 }
 
-export default App;
+const MyName = () => {
+  const age = 90;
+
+  return (
+    <div>
+      <h1>The Div Guy, {age}</h1>
+      <p>Bro</p>
+      <TestComponent></TestComponent>
+    </div>
+  );
+};
+
+class TestComponent extends Component {
+  render() {
+    return (
+      <div data-from-class="true">
+        <p>I live inside a class</p>
+      </div>
+    );
+  }
+}
