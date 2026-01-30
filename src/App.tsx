e5f76823b51289524fd4292145f162c802b958cf
Najam <realnajmiter@gmail.com>
2026-01-31 03:34:22 +0500

    states

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,44 @@
+import { useState } from 'react';
 import './App.css';
 
-// props -> children -> events
-
 export default function App() {
-  const handleClick = () => {
-    console.log('Here was clicked');
+  const [backgroundColor, setBackgroundColor] = useState<'red' | 'orange'>('red');
+  const [value, setValue] = useState(`Hello ${backgroundColor}`);
+
+  const handleClick = async () => {
+    // orange
+    setBackgroundColor((prev) => {
+      if (prev === 'red') return 'orange';
+      else return 'red';
+    });
+
+    setValue((prev) => {
+      return prev.includes('Hello') ? `Hi ${backgroundColor}` : `Hello ${backgroundColor}`;
+    });
   };
-
-  const handleInput = (e: any) => {
-    console.log(e.target.value);
-  };
-
-  return (
-    <main>
-      <input type="text" onInput={handleInput} />
-      <button onClick={handleClick}>Click Here</button>
-      <User username="" age={0} />
-      <div aria-hidden style={{ width: 200, height: 200, background: 'hotpink', filter: 'blur(100px)' }} />
-      {/* <Content></Content> */}
-    </main>
-  );
-}
-
-interface Props {
-  username: string;
-  age: number;
-  children?: React.ReactNode;
-}
-
-function User(params: Props) {
-  console.log(params);
-
-  const { username, age, children } = params;
-
-  // const {username, age} = {
-  //   username: 'some username',
-  //   age: 90,
-  // }
 
   return (
     <div>
-      <h1>{username}</h1>
-      <p>I am {age} years old</p>
-      {children}
+      <button onClick={handleClick} style={{ backgroundColor }}>
+        <h1>{value}</h1>
+      </button>
     </div>
   );
 }
 
-// function Content() {
-//   return (
-//     <div>
-//       <User age={91} username={'TheDivGuy'}>
-//         <div>
-//           <p>
-//             This <strong>is</strong> some extra content
-//           </p>
-//           <span>
-//             <span></span>
-//           </span>
-//         </div>
-//       </User>
+/* 
+let backgroundColor = 'red';
 
-//       <User username="Name" age={98}></User>
-//     </div>
-//   );
-// }
+function setBackgroundColor(color: string): void {
+  backgroundColor = color;
+}
+
+function useState(color: string): [string, (v: string) => void] {
+  setBackgroundColor(color);
+
+  return [color, setBackgroundColor];
+}
+
+const [bg, setBg] = useState('red')
+
+*/
