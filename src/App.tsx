42dfc9ab245f9a48face1d0fc436fa5ba089323d
Najam <realnajmiter@gmail.com>
2026-02-08 02:59:24 +0500

    `useCallback`

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
 import React from 'react';
 
+// function useCallback(cb: () => void, deps: []) {
+//   return React.useMemo(() => cb, deps);
+// }
+
 export default function App() {
   const [count, setCount] = React.useState(0);
-  const [count2, setCount2] = React.useState(0);
 
-  // const todos = [1, 2, 3];
-
-  const value = React.useMemo(() => {
+  const getValue = React.useCallback(() => {
     let total = 0;
     for (const i of Array(10_000_000).keys()) {
-      total += i + count2;
+      total += i;
     }
     return total;
-  }, [count2]);
-
-  // const getValue = () => {
-  //   let total = 0;
-  //   for (const i of Array(10_000_000).keys()) {
-  //     total += i;
-  //   }
-  //   return total;
-  // };
+  }, []);
 
   const handleClick = () => {
     setCount((p) => p + 1);
-    if (count % 2 === 0) {
-      setCount2((p) => p + 1);
-    }
   };
 
   return (
@@ -34,8 +24,7 @@ export default function App() {
       <button onClick={handleClick} className="p-3 rounded-xl bg-indigo-500 cursor-pointer">
         {count} +
       </button>
-      <h1 className="text-7xl font-mono">{value}</h1>
-      {/* <h1 className="text-7xl font-mono">{getValue()}</h1> */}
+      <h1 className="text-7xl font-mono">{getValue()}</h1>
     </div>
   );
 }
